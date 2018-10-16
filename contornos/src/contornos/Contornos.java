7a44248ed7622372b1030da862ecdacfcfc2a6a4
hposecarames <>
2018-10-16 08:56:29 +0200

    Prueba clone, pull, contornos de desarrollo

---
+++ contornos/src/contornos/Contornos.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package contornos;
+
+/**
+ *
+ * @author menuven
+ */
+public class Contornos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       System.out.println("Prueba Contornos");
+        // TODO code application logic here
+    }
+    
+}
